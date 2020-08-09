32b748f4afddf1eb9b5f30c82ac451610e5a559d
zhiluniao <huangshunle_gxu@163.com>
2020-08-09 11:38:54 +0800

    新增敏感词汇替换功能（rust echo函数返回替换敏感词后的句子）

--- node/app.js
+++ node/app.js
@@ -1,4 +1,4 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { say,echo } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
 const http = require('http');
 const url = require('url');
@@ -7,10 +7,12 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
+  if (queryObject['name']) {
     res.end(say(queryObject['name']) + '\n');
+  } else if(queryObject['sentence']){
+    res.end(echo(queryObject['sentence']) + '\n');
+  }else {
+    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName or curl http://${hostname}:${port}/?sentence=MySentence\n`);
   }
 });
 
